305f1338994ca76ff284439fd71c5c95584e6666
Ulises Peña <marculises14@gmail.com>
2022-09-13 13:21:58 -0500

    2nd Commit. Navbar added.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
+import { useEffect, useState } from "react";
 import "./App.css";
-import { Cover } from "./components/cover/Cover.js";
+import { Cover } from "./components/cover/Cover";
+import { Navbar } from "./components/navbar/Navbar";
 
 function App() {
+  // función de estado donde guardaremos donde es la posición de nuestro scroll.
+  const [scrollHeight, setScrollHeight] = useState(0);
+
+  const handleScroll = () => {
+    const position = window.scrollY;
+    setScrollHeight(position);
+  };
+
+  // Para que cada vez que hagamos scroll se actualice la Navbar:
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll);
+  }, [scrollHeight]);
+
   return (
     <div className="App">
+      <Navbar isScrolling={scrollHeight} />
       <Cover />
     </div>
   );
